1002c8f7eee635782f5f39af28c4e42f9a5a4964
d4vidglzr0 <67521763+d4vidglzr0@users.noreply.github.com>
2022-09-12 22:42:24 -0500

    Add files via upload

---
+++ programas criptografia/ejercicio 4/cesar_b.py
@@ -0,0 +1,54 @@
+#cifrado cesar inciso b
+
+#mensaje = input("introduce el mensaje a cifrar: ")
+mensaje = 'hola'
+
+
+A=[a,b,c,d,e,f,g,h,i,j,k,l,m,n,ñ,o,p,q,r,s,t,u,v,w,x,y,z]
+
+#llave = int(input("ingresa un numero entero positivo: "))
+llave = len(mensaje)
+
+MensajeTransformado = []
+cifrado = []
+C=[]
+
+desifrando=[]
+desifrado=[]
+D=[]
+
+
+#obteniendo mensaje en asccii
+for i in mensaje:
+	MensajeTransformado.append(ord(i))
+
+#print(MensajeTransformado)
+
+#cifrando desde asccii
+for j in MensajeTransformado:
+	cifrado.append(j+llave)
+
+#print(cifrado)
+#lista de nuevos caracteres mensaje cifrado
+for k in cifrado:
+	C.append(chr(k))
+
+
+print(MensajeTransformado,"\n",cifrado,"\n",C)
+
+#desifrar
+
+for x in C:
+	desifrando.append(ord(x))
+
+print("\n",desifrando)
+
+for y in desifrando:
+	desifrado.append(y-llave)
+
+print(desifrado)
+
+for z in desifrado:
+	D.append(chr(z))
+
+print(D)
