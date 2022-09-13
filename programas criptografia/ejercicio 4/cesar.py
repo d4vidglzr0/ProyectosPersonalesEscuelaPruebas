7e0dc732a5dcfba1c09947a3c3312c88c5d98bdf
d4vidglzr0 <67521763+d4vidglzr0@users.noreply.github.com>
2022-09-12 22:15:53 -0500

    Add files via upload

---
+++ programas criptografia/ejercicio 4/cesar.py
@@ -0,0 +1,50 @@
+#cifrado cesar inciso a
+
+#mensaje = input("introduce el mensaje a cifrar: ")
+mensaje = 'hola'
+#llave = int(input("ingresa un numero entero positivo: "))
+llave = 3
+
+MensajeTransformado = []
+cifrado = []
+C=[]
+
+desifrando=[]
+desifrado=[]
+D=[]
+
+
+#obteniendo mensaje en asccii
+for i in mensaje:
+	MensajeTransformado.append(ord(i))
+
+#print(MensajeTransformado)
+
+#cifrando desde asccii
+for j in MensajeTransformado:
+	cifrado.append(j+llave)
+
+#print(cifrado)
+#lista de nuevos caracteres mensaje cifrado
+for k in cifrado:
+	C.append(chr(k))
+
+
+print(MensajeTransformado,"\n",cifrado,"\n",C)
+
+#desifrar
+
+for x in C:
+	desifrando.append(ord(x))
+
+print("\n",desifrando)
+
+for y in desifrando:
+	desifrado.append(y-llave)
+
+print(desifrado)
+
+for z in desifrado:
+	D.append(chr(z))
+
+print(D)
